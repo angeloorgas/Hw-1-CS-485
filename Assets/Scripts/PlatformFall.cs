02e65fcd0441a2a994f7d7d94b9c0b6124da0072
Angelo Orgas <orgas002@cougars.csusm.edu>
2017-09-17 12:14:30 -0700

    First Commit.

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+	public float fallDelay = 1f;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+			//rb2d.bodyType = RigidbodyType2D.Dynamic;
+		Invoke("Fall", fallDelay);
+	}
+
+	void Fall()
+	{
+		rb2d.isKinematic = false;
+	}
+
+}
